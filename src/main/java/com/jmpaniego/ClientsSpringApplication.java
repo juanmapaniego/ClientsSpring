63fbd6456bf2726904fd85e80270ea1150d4b158
Juan Paniego <juan.paniego@synapse.com>
2021-02-20 11:56:11 -0300

    First commit

---
+++ src/main/java/com/jmpaniego/ClientsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.jmpaniego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientsSpringApplication.class, args);
+	}
+
+}
